83f2a37c74bd95b0741a3a4d9bc3e4c3c6f0f13f
211016759 <wes.bornor@ge.com>
2015-01-17 14:48:16 -0500

    display LED strand on measurement

--- milk-minder.ino
+++ milk-minder.ino
@@ -1,14 +1,31 @@
 //*****************************************************************************
 #include <SoftwareSerial.h>   
 #include <SmartThings.h>
+#include <Adafruit_NeoPixel.h>
 #include "Statistic.h" //http://playground.arduino.cc/Main/Statistics
 
+#define PIN_LED_STRIP     6
 #define PIN_LED           13
 #define PIN_THING_RX      3
 #define PIN_THING_TX      2
 
 SmartThingsCallout_t messageCallout;    // call out function forward decalaration
 SmartThings smartthing(PIN_THING_RX, PIN_THING_TX, messageCallout);  // constructor
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN_LED_STRIP, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
 
 bool isDebugEnabled;    // enable or disable debug in this example
 int stateLED;           // state to track last set value of LED
@@ -78,6 +95,9 @@ void setup() {
   }
   
   stats.clear();
+  
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
@@ -88,14 +108,27 @@ void loop() {
   setNetworkStateLED();
   
   // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  Serial.print("fsr1: ");
+  Serial.print("fsr0: ");
+  Serial.print(A0);
+  
+  
+  Serial.print(", fsr1: ");
   Serial.print(sensorValue);
   LEDbrightness = map(sensorValue, 0, 1023, 0, 255);
   
-  sensorValue2 = analogRead(sensorPin2);
+  sensorValue2 = analogRead(A2);
   Serial.print(", fsr2: ");
   Serial.print(sensorValue2);
+ 
+  Serial.print(", fsr3: ");
+  Serial.print(analogRead(A3));
+  
+  Serial.print(", fsr4: ");
+  Serial.print(analogRead(A4));
+  sensorValue = analogRead(A4);
+  
+  Serial.print(", fsr5: ");
+  Serial.print(analogRead(A5));
   
   //sensorValue = (sensorValue + sensorValue2)/2;
   //Serial.print(", TOTAL: ");
@@ -103,7 +136,7 @@ void loop() {
   
   // we'll need to change the range from the analog reading (0-1023) down to the range
   // used by analogWrite (0-255) with map!
-  sensorValue = map(sensorValue, 780, 860, 0, 100);
+  sensorValue = map(sensorValue, 0, 1000, 0, 100);
 
   //offset  middle tier reading
   //sensorValue = sensorValue >= 90 && sensorValue <= 95 ? map(sensorValue, 90, 95, 10, 99) : sensorValue;
@@ -127,24 +160,39 @@ void loop() {
 
   Serial.print(stats.pop_stdev(), 4);
  
-  if (stats.pop_stdev() > 5  || stats.count() >= 500) {
-    announceForce(sensorValue);
+  if (stats.pop_stdev() > 5  /*|| stats.count() >= 500*/) {
     stats.clear();
   }
   
-  if ( stats.count() < 5 ) {
-    announceForce(sensorValue);
+  if ( stats.count() < 2 ) {
+    
+    for(int i = 0; i < 1; i++){
+      delay(100);
+      if(sensorValue < 30) {
+        announceForce(sensorValue);
+        theaterChase(strip.Color(255, 0, 0), 10); // red
+        
+      } else if(sensorValue < 60) {
+          announceForce(sensorValue);
+          theaterChase(strip.Color(0, 0, 255), 10); // blue
+      } else {
+        announceForce(sensorValue);
+        theaterChase(strip.Color(0, 255, 0), 50); // green
+      }
+    }
+  } else if ( stats.count() == 5) {
+   colorWipe(strip.Color(0, 0, 0), 1); // Off 
   }
   else {
     // turn the ledPin off:
     digitalWrite(ledPin, LOW);
   }
   
+  //strandTest();
   
   
   // stop the program for <sensorValue> milliseconds:
-  delay(sensorValue);
-  
+  //delay(sensorValue);
   
   Serial.println();
 }
@@ -157,6 +205,9 @@ void announceForce(int force) {
     
     // turn the ledPin on
     digitalWrite(ledPin, LEDbrightness);
+    
+    //strandBlip();
+    
 }
 
 void networkTrafficLED() {
@@ -187,4 +238,112 @@ void messageCallout(String message)
 
 }
 
+void strandBlip(){
+  
+  colorWipe(strip.Color(0, 255, 0), 0.5); // Green
+  colorWipe(strip.Color(255, 0, 0), 0.1); // Red
+  colorWipe(strip.Color(0, 0, 0), 1); // Off
+}
+
+void strandTest(){
+
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  // Send a theater pixel chase in...
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127,   0,   0), 50); // Red
+  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+
+  rainbow(20);
+  rainbowCycle(20);
+  theaterChaseRainbow(50);
+  
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
 
